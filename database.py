69e4ca02cd0800aebf03c614d3931aa1e3305b76
Kaan Kanat <kaankanat4@hotmail.com>
2023-04-04 17:37:10 +0300

    done

--- database.py
+++ database.py
@@ -3,64 +3,74 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS kullanicilar
-            (id INTEGER PRIMARY KEY AUTOINCREMENT,
-            isim TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE,
-            yas INTEGER NOT NULL)''')
+c.execute('''
+          CREATE TABLE IF NOT EXISTS kullanici
+          (isim TEXT, email TEXT, yas INTEGER)
+          ''')
+
+print("Kullanici veritabanina baglandi...")
 
 while True:
-    print("1. Ekleme\n2. Silme\n3. Güncelleme\n4. Görüntüleme\n5. Çıkış\n")
-    secim = input("Yapmak istediğiniz işlemi seçiniz: ")
+    print(
+        "\nLutfen yapmak istediginiz islemi secin:\n1. Kullanici ekleme\n2. Kullanici silme\n3. Kullanici guncelleme\n4. Kullanici goruntuleme\n5. Cikis")
+    secim = input("Seciminiz: ")
 
     if secim == "1":
-        isim = input("İsim: ")
-        email = input("Email: ")
-        yas = int(input("Yaş: "))
-        c.execute("INSERT INTO kullanicilar (isim, email, yas) VALUES (?, ?, ?)", (isim, email, yas))
-        print("Kullanıcı eklendi.\n" + "-" * 50)
+        isim = input("Isim: ").lower()
+        email = input("Email: ").lower()
+        yas = int(input("Yas: "))
+        c.execute('INSERT INTO kullanici VALUES (?, ?, ?)', (isim, email, yas))
+        conn.commit()
+        print("\nKullanici basariyla eklendi.")
 
     elif secim == "2":
-        isim = input("Silmek istediğiniz kullanıcının ismini giriniz: ")
-        c.execute("SELECT * FROM kullanicilar WHERE isim=?", (isim,))
-        result = c.fetchone()
-        if result:
-            c.execute("DELETE FROM kullanicilar WHERE isim=?", (isim,))
-            print("Kullanıcı silindi.\n" + "-" * 50)
+        isim = input("Silmek istediginiz kullanici ismi: ").lower()
+        c.execute('SELECT * FROM kullanici WHERE isim = ?', (isim,))
+        data = c.fetchall()
+        if len(data) == 0:
+            print("\nBu isim sistemde bulunamamistir.")
         else:
-            print("Bu isim sistemde bulunamamıştır.\n" + "-" * 50)
+            c.execute('DELETE FROM kullanici WHERE isim = ?', (isim,))
+            conn.commit()
+            print("\nKullanici basariyla silindi.")
 
     elif secim == "3":
-        isim = input("Güncellemek istediğiniz kullanıcının ismini giriniz: ")
-        c.execute("SELECT * FROM kullanicilar WHERE isim=?", (isim,))
-        result = c.fetchone()
-        if result:
-            email = input("Yeni Email: ")
-            yas = int(input("Yeni Yaş: "))
-            c.execute("UPDATE kullanicilar SET email=?, yas=? WHERE isim=?", (email, yas, isim))
-            print("Kullanıcı güncellendi.\n" + "-" * 50)
+        isim = input("Guncellemek istediginiz kullanici ismi: ").lower()
+        c.execute('SELECT * FROM kullanici WHERE isim = ?', (isim,))
+        data = c.fetchall()
+        if len(data) == 0:
+            print("\nBu isim sistemde bulunamamistir.")
         else:
-            print("Bu isim sistemde bulunamamıştır.\n" + "-" * 50)
+            print("\nMevcut bilgiler:")
+            for row in data:
+                print("Isim:", row[0])
+                print("Email:", row[1])
+                print("Yas:", row[2])
+            yeni_isim = input("Yeni isim: ").lower()
+            yeni_email = input("Yeni email: ").lower()
+            yeni_yas = int(input("Yeni yas: "))
+            c.execute('UPDATE kullanici SET isim = ?, email = ?, yas = ? WHERE isim = ?',
+                      (yeni_isim, yeni_email, yeni_yas, isim))
+            conn.commit()
+            print("\nKullanici basariyla guncellendi.")
 
     elif secim == "4":
-        c.execute("SELECT * FROM kullanicilar")
-        result = c.fetchall()
-        if result:
-            for row in result:
-                print("ID:", row[0])
-                print("İsim:", row[1])
-                print("Email:", row[2])
-                print("Yaş:", row[3])
-                print("-" * 50)
+        c.execute('SELECT * FROM kullanici')
+        data = c.fetchall()
+        if len(data) == 0:
+            print("\nVeri tabani su anda bos.")
         else:
-            print("Veri tabanı şu anda boş.\n" + "-" * 50)
+            print("\nKullanici bilgileri:")
+            for row in data:
+                print("Isim:", row[0])
+                print("Email:", row[1])
+                print("Yas:", row[2])
 
     elif secim == "5":
-        print("Çıkış yapılıyor...")
+        print("Program kapatiliyor...")
         break
 
     else:
-        print("Lütfen geçerli bir seçenek giriniz.\n" + "-" * 50)
+        print("Gecersiz secim. Lutfen tekrar deneyin.")
 
-conn.commit()
-conn.close()
+conn.close
