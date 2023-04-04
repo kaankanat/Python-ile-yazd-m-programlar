3d7a4e9b1396a4c634e52dce1ad0b8dfd5b42d59
Kaan Kanat <kaankanat4@hotmail.com>
2023-04-04 18:08:25 +0300

    done

--- database.py
+++ database.py
@@ -1,84 +1,80 @@
 import sqlite3
 
-def veri_ekle():
-    isim = input("İsim: ")
-    email = input("E-posta: ")
-    yas = input("Yaş: ")
-    cursor.execute("INSERT INTO kisiler (isim, email, yas) VALUES (?, ?, ?)", (isim, email, yas))
-    conn.commit()
-    print("Kişi başarıyla eklendi.")
+# Veritabanı bağlantısı oluşturma
+conn = sqlite3.connect('veritabani.db')
 
-def veri_sil():
-    isim = input("Silmek istediğiniz kişinin ismini girin: ")
-    cursor.execute("SELECT * FROM kisiler WHERE isim=?", (isim,))
-    row = cursor.fetchone()
-    if row is None:
-        print("Böyle bir kişi bulunamadı.")
-    else:
-        cursor.execute("DELETE FROM kisiler WHERE isim=?", (isim,))
-        conn.commit()
-        print("Kişi başarıyla silindi.")
+# Veritabanındaki tablo oluşturma
+conn.execute('''CREATE TABLE IF NOT EXISTS kisiler
+             (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+             ISIM TEXT NOT NULL,
+             YAS INT NOT NULL,
+             EMAIL TEXT NOT NULL);''')
 
-def veri_guncelle():
-    isim = input("Güncellemek istediğiniz kişinin ismini girin: ")
-    cursor.execute("SELECT * FROM kisiler WHERE isim=?", (isim,))
-    row = cursor.fetchone()
-    if row is None:
-        print("Böyle bir kişi bulunamadı.")
-    else:
-        email = input("Yeni e-posta adresi: ")
-        yas = input("Yeni yaş: ")
-        cursor.execute("UPDATE kisiler SET email=?, yas=? WHERE isim=?", (email, yas, isim))
-        conn.commit()
-        print("Kişi başarıyla güncellendi.")
-
-def veri_goruntule():
-    cursor.execute("SELECT * FROM kisiler")
-    rows = cursor.fetchall()
-    if len(rows) == 0:
-        print("Veri tabanı boş.")
-    else:
-        for row in rows:
-            print(f"{row[0]} - {row[1]} - {row[2]}")
-
-# Veri tabanı bağlantısı oluştur
-conn = sqlite3.connect("kisiler.db")
-
-# Cursor oluştur
-cursor = conn.cursor()
-
-# Tablo oluştur
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS kisiler (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        isim TEXT NOT NULL,
-        email TEXT NOT NULL,
-        yas INT NOT NULL
-    )
-""")
-
-# Menüyü göster
 while True:
+    # Kullanıcıdan işlem seçimini alınması
     print("Veri Tabanı Programı")
-    print("1 - Veri tabanına kişi eklemek")
-    print("2 - Veri tabanından kişi silmek")
-    print("3 - Veri tabanındaki kişiyi güncellemek")
-    print("4 - Veri tabanını görüntülemek")
-    print("q - Veri tabanından çıkmak")
-    secim = input("Seçiminiz: ")
+    print("Kişi eklemek: 1")
+    print("Kişi silmek: 2")
+    print("Kişi güncellemek: 3")
+    print("Veri tabanını görüntülemek: 4")
+    print("Programdan çıkmak: q")
+    secim = input("Yapmak istediğiniz işlemi seçin: ")
 
+    # Veritabanına kişi eklemek
     if secim == "1":
-        veri_ekle()
-    elif secim == "2":
-        veri_sil()
-    elif secim == "3":
-        veri_guncelle()
-    elif secim == "4":
-        veri_goruntule()
-    elif secim == "q":
+        isim = input("Kişinin adını girin: ")
+        yas = int(input("Kişinin yaşını girin: "))
+        email = input("Kişinin e-posta adresini girin: ")
+        conn.execute("INSERT INTO kisiler (ISIM, YAS, EMAIL) VALUES (?, ?, ?)", (isim, yas, email))
+        conn.commit()
+        print("Kişi başarıyla eklendi.")
+
+    # Veritabanından kişi silmek
+    elif secim == '2':
+        if len(kisiler) == 0:
+            print("Veri tabanı boş.")
+        else:
+            silinecek_isim = input("Silinecek kişinin ismi: ")
+            bulundu = False
+            for i in range(len(kisiler)):
+                if kisiler[i]["isim"] == silinecek_isim:
+                    bulundu = True
+                    kisiler.pop(i)
+                    print(f"{silinecek_isim} isimli kişi başarıyla silindi.")
+                    break
+            if not bulundu:
+                print(f"{silinecek_isim} isimli kişi veri tabanında bulunamadı.")
+
+    elif secim == '3':
+        if len(kisiler) == 0:
+            print("Veri tabanı boş.")
+        else:
+            guncellenecek_isim = input("Güncellenecek kişinin ismi: ")
+            bulundu = False
+            for i in range(len(kisiler)):
+                if kisiler[i]["isim"] == guncellenecek_isim:
+                    bulundu = True
+                    yeni_isim = input("Yeni isim: ")
+                    yeni_email = input("Yeni email: ")
+                    yeni_yas = input("Yeni yaş: ")
+                    kisiler[i] = {"isim": yeni_isim, "email": yeni_email, "yas": yeni_yas}
+                    print("Kişi başarıyla güncellendi.")
+                    break
+            if not bulundu:
+                print(f"{guncellenecek_isim} isimli kişi veri tabanında bulunamadı.")
+
+    elif secim == '4':
+        if len(kisiler) == 0:
+            print("Veri tabanı boş.")
+        else:
+            print("Veri tabanındaki kişiler:")
+            for kisi in kisiler:
+                print(f"İsim: {kisi['isim']}, Email: {kisi['email']}, Yaş: {kisi['yas']}")
+
+    elif secim == 'q':
+        print("Programdan çıkılıyor...")
         break
+
     else:
         print("Geçersiz seçim. Lütfen tekrar deneyin.")
 
-# Veri tabanı bağlantısını kapat
-conn.close()
