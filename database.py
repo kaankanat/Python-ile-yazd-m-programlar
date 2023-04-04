a9a9f46c6da6521507e42d3443d8242b2b10b073
Kaan Kanat <kaankanat4@hotmail.com>
2023-04-04 18:04:22 +0300

    done

--- database.py
+++ database.py
@@ -1,88 +1,78 @@
-# --------------------------------------------
-# Kullanıcıdan isim, yaş, e-posta bilgileri alınıyor
-# --------------------------------------------
-def bilgi_gir():
-    isim = input("İsim: ")
-    yas = input("Yaş: ")
-    email = input("E-posta: ")
-    return isim, yas, email
+# Veri Tabanı Programı
 
-
-# --------------------------------------------
-# Database'e bilgi ekleme fonksiyonu
-# --------------------------------------------
-def ekle(isim, yas, email, db):
-    db[isim] = {"Yaş": yas, "E-posta": email}
-    print(f"{isim} bilgileri database'e eklendi.")
-
-
-# --------------------------------------------
-# Database'den bilgi silme fonksiyonu
-# --------------------------------------------
-def sil(isim, db):
-    if isim in db:
-        del db[isim]
-        print(f"{isim} bilgileri database'den silindi.")
-    else:
-        print(f"{isim} database'de bulunamadı.")
-
-
-# --------------------------------------------
-# Database'deki bilgileri güncelleme fonksiyonu
-# --------------------------------------------
-def guncelle(isim, db):
-    if isim in db:
-        print(f"{isim} bilgileri güncelleniyor.")
-        yeni_yas = input("Yeni Yaş: ")
-        yeni_email = input("Yeni E-posta: ")
-        db[isim]["Yaş"] = yeni_yas
-        db[isim]["E-posta"] = yeni_email
-        print(f"{isim} bilgileri güncellendi.")
-    else:
-        print(f"{isim} database'de bulunamadı.")
-
-
-# --------------------------------------------
-# Database'deki bilgileri görüntüleme fonksiyonu
-# --------------------------------------------
-def goruntule(db):
-    if not db:
-        print("Database boş.")
-    else:
-        for isim, bilgiler in db.items():
-            print(f"İsim: {isim}")
-            for bilgi_adi, bilgi_degeri in bilgiler.items():
-                print(f"{bilgi_adi}: {bilgi_degeri}")
-            print("\n")
-
-
-# --------------------------------------------
-# Ana kod bloğu
-# --------------------------------------------
-database = {}
+database = []
 
 while True:
-    print("\nNe yapmak istersin?")
-    print("Ekle: e")
-    print("Sil: s")
-    print("Güncelle: g")
-    print("Görüntüle: v")
-    print("Çıkış: q")
+    print("--------------------")
+    print("Veri tabanına kişi eklemek: 1")
+    print("Veri tabandan kişi silmek: 2")
+    print("Veri tabanındaki kişiyi güncellemek: 3")
+    print("Veri tabanını görüntülemek: 4")
+    print("Veri tabanından çıkmak: q")
+    print("--------------------")
 
-    secim = input("Seçiminiz: ")
+    # Kullanıcıdan seçim yapmasını istiyoruz
+    secim = input("Yapmak istediğiniz işlemin numarasını girin: ")
 
-    if secim == "e":
-        isim, yas, email = bilgi_gir()
-        ekle(isim, yas, email, database)
-    elif secim == "s":
-        isim = input("Silinecek kişinin ismi: ")
-        sil(isim, database)
-    elif secim == "g":
-        isim = input("Güncellenecek kişinin ismi: ")
-        guncelle(isim, database)
-    elif secim == "v":
-        goruntule(database)
+    # Seçime göre ilgili işlem yapılıyor
+    if secim == "1":
+        # Yeni kişi ekleme
+        print("Yeni kişi ekleyin:")
+        isim = input("İsim: ")
+        yas = input("Yaş: ")
+        email = input("E-posta: ")
+        database.append({"isim": isim, "yas": yas, "email": email})
+        print("Kişi veri tabanına eklendi.")
+
+    elif secim == "2":
+        # Kişi silme
+        if len(database) == 0:
+            print("Veri tabanı boş.")
+            continue
+        isim = input("Silmek istediğiniz kişinin adını girin: ")
+        bulundu = False
+        for kisi in database:
+            if kisi["isim"] == isim:
+                database.remove(kisi)
+                bulundu = True
+                print("Kişi veri tabanından silindi.")
+                break
+        if not bulundu:
+            print("Bu isimde bir kişi veri tabanında yok.")
+
+    elif secim == "3":
+        # Kişi güncelleme
+        if len(database) == 0:
+            print("Veri tabanı boş.")
+            continue
+        isim = input("Güncellemek istediğiniz kişinin adını girin: ")
+        bulundu = False
+        for kisi in database:
+            if kisi["isim"] == isim:
+                yeni_isim = input("Yeni isim: ")
+                yeni_yas = input("Yeni yaş: ")
+                yeni_email = input("Yeni e-posta: ")
+                kisi["isim"] = yeni_isim
+                kisi["yas"] = yeni_yas
+                kisi["email"] = yeni_email
+                bulundu = True
+                print("Kişi veri tabanında güncellendi.")
+                break
+        if not bulundu:
+            print("Bu isimde bir kişi veri tabanında yok.")
+
+    elif secim == "4":
+        # Veri tabanını görüntüleme
+        if len(database) == 0:
+            print("Veri tabanı boş.")
+            continue
+        for kisi in database:
+            print(f"İsim: {kisi['isim']}, Yaş: {kisi['yas']}, E-posta: {kisi['email']}")
+
     elif secim == "q":
+        # Çıkış
+        print("Program sonlandırılıyor.")
         break
+
     else:
-        print("Geçersiz seçim. Lütfen tekrar deneyin.")
+        print("Geçersiz seçim.")
